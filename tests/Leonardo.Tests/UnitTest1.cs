c5d05448add73ac9661d64a0208001873a8baaa7
Hamed Cédric SYLLA <syllahamed26@yahoo.com>
2023-10-24 16:01:45 +0200

    change test to work

--- tests/Leonardo.Tests/UnitTest1.cs
+++ tests/Leonardo.Tests/UnitTest1.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.EventHandlers;
+
 namespace Leonardo.Tests;
 
 public class UnitTest1
 {
+    
     [Fact]
     public async void Test1()
     {
-        var results = await Fibonacci.RunAsync(new[] { "40", "41", "42" });
+        var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+        var dataBaseName = Guid.NewGuid().ToString();
+        builder.UseInMemoryDatabase(dataBaseName);
+
+        var options = builder.Options;
+        var fibonacciDataContext = new FibonacciDataContext(options);
+        await fibonacciDataContext.Database.EnsureCreatedAsync();
+        
+        var results = await new Fibonacci(fibonacciDataContext).RunAsync(new[] { "40", "41", "42" });
         Assert.Equal(3, results.Count);
         Assert.Equal(165580141, results[0]);
         Assert.Equal(267914296, results[1]);
